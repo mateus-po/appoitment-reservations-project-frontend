ed3b2381a0549410c3eb019e0fcc6b806948a9ca
Mateusz Porębski <mateusz.porebski99@gmail.com>
2025-01-14 20:10:48 +0100

    add consultations form validations

--- src/app/components/doctor/add-consultations/add-consultations.component.ts
+++ src/app/components/doctor/add-consultations/add-consultations.component.ts
@@ -3,6 +3,7 @@ import { AddAvailabilityService } from "../../../services/availability/addAvaila
 import { ReloadService } from "../../../services/reload/reload.service";
 import { FormsModule } from "@angular/forms";
 import { NgIf, NgFor } from "@angular/common";
+import dayjs from "dayjs";
 
 @Component({
   selector: "app-add-consultations",
@@ -88,7 +89,7 @@ export class AddConsultationsComponent implements OnInit {
       .saveOneTimeAvailability(this.oneTimeAvailability)
       .subscribe(
         () => {
-          alert("One-time availability saved!")
+          alert("One-time availability saved!");
           this.reloadService.triggerReload();
         },
         (error) => console.error("Error saving one-time availability", error)
@@ -104,4 +105,103 @@ export class AddConsultationsComponent implements OnInit {
       (error) => console.error("Error saving absence", error)
     );
   }
+
+  isCyclicalAvailabilityFormValid(): boolean {
+    const { startDate, endDate, days, timeSlots } = this.cyclicalAvailability;
+
+    if (
+      startDate == "" ||
+      endDate == "" ||
+      days.length == 0 ||
+      timeSlots.length == 0
+    ) {
+      return false;
+    }
+    if (
+      dayjs(startDate)
+        .hour(0)
+        .minute(0)
+        .second(0)
+        .millisecond(0)
+        .isBefore(dayjs().add(-1, 'day')) ||
+      dayjs(endDate).isBefore(dayjs(startDate))
+    ) {
+      return false
+    }
+
+      return this.areTimeSlotsValid(timeSlots);
+  }
+
+
+  isOneTimeAvailibilityFormValid():boolean {
+    const {date, timeSlots} = this.oneTimeAvailability
+
+    if (
+      date == "" ||
+      timeSlots.length == 0
+    ) {
+      return false;
+    }
+    if (
+      dayjs(date)
+        .hour(0)
+        .minute(0)
+        .second(0)
+        .millisecond(0)
+        .isBefore(dayjs().add(-1, 'day'))
+    ) {
+      return false
+    }
+
+      return this.areTimeSlotsValid(timeSlots);
+  }
+
+  isAbsenceFormValid():boolean {
+    const {startDate, endDate} = this.absence
+
+    if (
+      startDate == "" ||
+      endDate == "" 
+    ) {
+      return false;
+    }
+    if (
+      dayjs(startDate)
+        .hour(0)
+        .minute(0)
+        .second(0)
+        .millisecond(0)
+        .isBefore(dayjs().add(-1, 'day')) ||
+      dayjs(endDate).isBefore(dayjs(startDate))
+    ) {
+      return false
+    }
+
+      return true;
+  }
+
+  areTimeSlotsValid(timeSlots: { start: string; end: string }[]): boolean {
+    let covered_timeslots:number[] = []
+
+    for (let timeSlot of timeSlots) {
+      if (timeSlot.start == '' || timeSlot.end == '') return false
+      let startTimeSlot = this.parseHourToTimeSlot(timeSlot.start)
+      const endTimeSlot = this.parseHourToTimeSlot(timeSlot.end)
+      if (endTimeSlot <= startTimeSlot) return false
+
+      while(startTimeSlot < endTimeSlot) {
+        if (covered_timeslots.includes(startTimeSlot)) return false
+        covered_timeslots.push(startTimeSlot)
+        startTimeSlot += 1
+      }
+    }
+
+    return true
+  }
+
+  parseHourToTimeSlot(timeslot: string):number {
+    return parseInt(timeslot.split(':')[0]) * 2 + (timeslot.split(':')[1] == '30' ? 1 : 0)
+  }
+
+  
 }
